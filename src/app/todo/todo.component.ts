c1fc1efabf0287cf41b6f771f8b1e1ab856a876c
gilalk <86377486+gilalk@users.noreply.github.com>
2022-03-24 00:14:54 +0200

    Basic Add functionality

---
+++ src/app/todo/todo.component.ts
@@ -0,0 +1,31 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+@Component({
+  selector: 'app-todo',
+  templateUrl: './todo.component.html',
+  styleUrls: ['./todo.component.css']
+})
+export class TodoComponent implements OnInit {
+
+  // An array for the tasks
+  tasks: string[] = [];
+
+  constructor() { 
+    this.tasks = [
+      'task 1',
+      'task 2',
+      'task 3',
+    ]
+  }
+
+  ngOnInit(): void {
+  }
+
+  handleSubmit(addForm:NgForm) {
+    let newTask = addForm.value.task;
+    this.tasks.push(newTask)
+    addForm.resetForm();
+  }
+
+}
