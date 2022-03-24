31c84e6de913f13cc5ee8e1e7e34543a34fb877b
gilalk <86377486+gilalk@users.noreply.github.com>
2022-03-24 09:24:43 +0200

    making it table and adding update

--- src/app/todo/todo.component.ts
+++ src/app/todo/todo.component.ts
@@ -1,6 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
 
+export interface Task {
+  name: string;
+  isUpdated: boolean;
+}
+
 @Component({
   selector: 'app-todo',
   templateUrl: './todo.component.html',
@@ -9,7 +14,7 @@ import { NgForm } from '@angular/forms';
 export class TodoComponent implements OnInit {
 
   // An array for the tasks
-  tasks: string[] = [];
+  tasks: Task[] = [];
 
   constructor() { 
   }
@@ -18,13 +23,27 @@ export class TodoComponent implements OnInit {
   }
 
   handleSubmit(addForm:NgForm) {
-    let newTask = addForm.value.task;
+    let newTask = {name: addForm.value.task, isUpdated: false};
     this.tasks.push(newTask)
     addForm.resetForm();
   }
 
   handleRemove(t:string){
-    this.tasks = this.tasks.filter((myTask) => myTask != t);
+    this.tasks = this.tasks.filter((myTask: Task) => myTask.name != t);
   }
 
+  handleUpdate(t:Task){
+    t.isUpdated = true;
+  }
+
+  handleFinishUpdate(oldName: string, newTaskName: string) {
+    // get task from array to new variable
+    let updatedTask: Task = this.tasks.filter((t) => t.name === oldName)[0];
+
+    //Now change the name
+    updatedTask.name = newTaskName;
+
+    //canceling update mode
+    updatedTask.isUpdated = false;
+  }
 }
